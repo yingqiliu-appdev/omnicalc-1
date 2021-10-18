e792ad6acfa9b23aeafbe38e0a5307e3d3c3d554
yingqi0408 <ying_liu@chicagobooth.edu>
2021-10-18 14:01:11 +0000

    Square form looks good doesn't work yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+    
+  end
+
+  def method_name
+    
+  end
+
+  def method_name
+    
+  end
+
+
+
 
 end
