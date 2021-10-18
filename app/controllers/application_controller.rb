ef8e96db2f80bf808d4b5460395e69cc32309f20
yingqi0408 <ying_liu@chicagobooth.edu>
2021-10-18 16:23:01 +0000

    square root is good to go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,30 @@ class ApplicationController < ActionController::Base
 
   end
 
-  def method_name
+  def blank_square_root_form
+    
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+
+  end
+
+  def calculate_square_root
+    @num_sqrt = params.fetch("number_sqrt").to_f
+    @sqrt_calc = @num_sqrt ** (0.5)
+
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+    
+  end
+
+
+
+
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+    render({ :template => "calculation_templates/rand_results.html.erb"})
     
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "application", :action => "blank_square_form"})
 
   get("/square/results", { :controller => "application", :action => "calculate_square"})
+  
+  get("/square_root/new", { :controller => "application", :action => "blank_square_root_form"})
+
+  get("/square_root/results", { :controller => "application", :action => "calculate_square_root"})
+
+
+
+  get("/random/results", { :controller => "application", :action => "calculate_random"})
 
 end
